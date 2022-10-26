04c2dbec137b66a321a22dda7701b3cbea1c0833
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-10-26 17:42:11 +0100

    fixing command string

--- server/mydsl/bin/generator.sh
+++ server/mydsl/bin/generator.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 inputFile=$2
 typeofbuild="$1"
+all="All"
 outputDir=$3
 APP_HOME="`pwd -P`"
 
 rm $APP_HOME/generator.properties
 rm -R $APP_HOME/src-gen
 
-if ["$1"!="All"]; then
+if [ "$typeofbuild" != "$all" ]; then
     echo "PLATFORM=$typeofbuild" > $APP_HOME/generator.properties
 fi
 
