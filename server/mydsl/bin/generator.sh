e07b03fc33b76d813b0be10e7f5ec1ae60abc81e
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-10-26 17:29:18 +0100

    added automatic tasks

--- server/mydsl/bin/generator.sh
+++ server/mydsl/bin/generator.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 inputFile=$2
-typeofbuild=$1
+typeofbuild="$1"
 outputDir=$3
 APP_HOME="`pwd -P`"
 
 rm $APP_HOME/generator.properties
 rm -R $APP_HOME/src-gen
 
-if ["$1" != "All"]
-then
+if [$1 != "All"]; then
     echo "PLATFORM=$typeofbuild" > $APP_HOME/generator.properties
 fi
 
