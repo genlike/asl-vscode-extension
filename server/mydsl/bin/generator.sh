c85b2c1c0b6c1c1b0be83af0222d67627bcc92a5
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-10-26 16:28:47 +0100

    fixing command string

--- server/mydsl/bin/generator.sh
+++ server/mydsl/bin/generator.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 inputFile=$2
 typeofbuild=$1
 outputDir=$(dirname "$inputFile")
@@ -7,5 +7,5 @@ APP_HOME="`pwd -P`"
 rm $APP_HOME/generator.proprieties
 echo "PLATFORM=$typeofbuild" > $APP_HOME/generator.proprieties
 
-exec "java -jar generator.jar" "$2"
-mv  -R $APP_HOME/src-gen $outputDir
+java -jar generator.jar $2
+mv -R $APP_HOME/src-gen $outputDir
