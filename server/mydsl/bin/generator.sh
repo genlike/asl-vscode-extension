4de7f075b95f803706e844d1f631e1c55fec3183
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-10-26 16:58:25 +0100

    remove src-gen

--- server/mydsl/bin/generator.sh
+++ server/mydsl/bin/generator.sh
@@ -5,6 +5,7 @@ outputDir=$3
 APP_HOME="`pwd -P`"
 
 rm $APP_HOME/generator.proprieties
+rm -R $APP_HOME/src-gen
 echo "PLATFORM=$typeofbuild" > $APP_HOME/generator.proprieties
 
 java -jar generator.jar $2
