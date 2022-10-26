3e1d9865fe8e17d0320d49f3e2d79c89dc1b7dd7
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-10-26 17:09:12 +0100

    added automatic tasks

--- server/mydsl/bin/generator.sh
+++ server/mydsl/bin/generator.sh
@@ -4,9 +4,9 @@ typeofbuild=$1
 outputDir=$3
 APP_HOME="`pwd -P`"
 
-rm $APP_HOME/generator.proprieties
+rm $APP_HOME/generator.properties
 rm -R $APP_HOME/src-gen
-echo "PLATFORM=$typeofbuild" > $APP_HOME/generator.proprieties
+echo "PLATFORM=$typeofbuild" > $APP_HOME/generator.properties
 
 java -jar generator.jar $2
 mv $APP_HOME/src-gen $outputDir
