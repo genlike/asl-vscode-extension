75f2b2d2eac130dfde05451b6003a8412fa77e12
GenLike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2022-11-16 11:23:20 +0000

    added custom command execution

--- src/asl-commands-extension.ts
+++ src/asl-commands-extension.ts
@@ -5,17 +5,17 @@ export class ASLCustomCommands {
     static registerCommands(){
         console.log('REGISTERED COMMANDS: asl.genie and asl.zip');
         vscode.commands.registerCommand('asl.genie.import',this.genieCallBack);
-        vscode.commands.registerCommand('asl.zip.import',this.genieCallBack);
+        vscode.commands.registerCommand('asl.zip.import',this.zipCallBack);
     } 
 
-    static genieCallBack(context: any[]){
+    static genieCallBack(...context: any[]){
         console.log("GenieCallback");
         for(const element of context){
             console.log(element);
         }
     }
 
-    static zipCallBack(context: any[]){
+    static zipCallBack(...context: any[]){
         console.log("ZipCallback");
         for(const element of context){
             console.log(element);
