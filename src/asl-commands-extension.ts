bd4be5a2f599a254536830c50ac3c8ace84326cc
genlike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2023-03-08 12:41:53 +0000

    added command

---
+++ src/asl-commands-extension.ts
@@ -0,0 +1,74 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as cp from "child_process";
+
+
+export class ASLCustomCommands implements vscode.Disposable {
+
+    context: vscode.ExtensionContext;
+    constructor(context: vscode.ExtensionContext){
+        this.context = context;
+    }
+    dispose() {
+        //
+    }
+
+    registerCommands(){
+        console.log('REGISTERED COMMANDS: asl.genie and asl.zip');
+        //vscode.commands.registerCommand('genie.import',this.genieCallBack);
+        vscode.commands.registerCommand('zip.import',this.zipCallBack, this);
+        vscode.commands.registerCommand('genio.export',this.exportGenieCallBack, this);
+    } 
+
+    genieCallBack(...context: any[]){
+        console.log("GenieCallback");
+    }
+
+    zipCallBack(...context: any[]){
+        console.log("ZipCallback");
+        let fileUri = context[0];
+        console.log(fileUri);
+        const askForOutputFile:vscode.InputBoxOptions = {
+            prompt:"What is the output file name?"
+        }
+        
+        const askForPlatform:vscode.QuickPickOptions = {
+            placeHolder:"PLATFORM",
+            canPickMany: false 
+        }
+//             const askForOutputFile:vscode.InputBoxOptions = {
+//                 prompt:"What is the output filename?"
+//             }
+            
+             const outputfile = vscode.window.showInputBox(askForOutputFile);
+             const outputPlatform = vscode.window.showQuickPick(['GENIO'], askForPlatform);
+            vscode.window.showInformationMessage('Generating for: ' + outputfile + "/" + outputPlatform + "/");
+        //let generatorPath = this.context.asAbsolutePath(path.join('server', 'mydsl', 'bin','generator.sh'));
+        console.log(outputfile + " "+  outputPlatform);
+    }
+
+
+    exportGenieCallBack(...callcontext: any[]){
+        console.log("exportGenie");
+        let fileUri = callcontext[0];
+        console.log(fileUri);
+        console.log("exportGenie-context");
+        console.log(callcontext.length)
+        console.log(callcontext)
+
+        let generatorPath = this.context.asAbsolutePath(path.join('server', 'asl', 'bin','generator.sh'));
+        let generatorType = 'Genio';
+        
+        const workspaceRoot = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+        console.log(workspaceRoot)
+	    if (!workspaceRoot) {
+		    return;
+	    }
+
+        const commandString :string = `${generatorPath} ${generatorType} ${fileUri.path} ${workspaceRoot}`
+        console.log(commandString);
+        console.log("exportGenie-2-");
+        cp.execSync(commandString);
+    }
+}
