baa3b30f67c609035d0eaa8e1ff56859ba96a2f1
genlike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2023-03-15 11:50:09 +0000

    debug run java

--- server/asl/bin/generator.sh
+++ server/asl/bin/generator.sh
@@ -13,6 +13,7 @@ if [ "$typeofbuild" != "$all" ]; then
 fi
 
 cd $APP_HOME
+echo "$APP_HOME"
 echo "java -cp $APP_HOME/../lib/org.itlingo.asl.ide-1.0.0-SNAPSHOT-ls.jar org.itlingo.asl.generator.Main $2"
 java -cp $APP_HOME/../lib/org.itlingo.asl.ide-1.0.0-SNAPSHOT-ls.jar org.itlingo.asl.generator.Main $2
 mv $outputDir/src-gen $outputDir/src-gen-$(date +%s)
