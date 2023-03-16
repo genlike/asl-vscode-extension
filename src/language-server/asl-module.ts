a84ca1f94ffd14db4c3af9832433034c0c8384b8
genlike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2023-03-16 16:55:41 +0000

    changed validator

--- src/language-server/asl-module.ts
+++ src/language-server/asl-module.ts
@@ -3,7 +3,7 @@ import {
     LangiumServices, LangiumSharedServices, Module, PartialLangiumServices
 } from 'langium';
 import { AslGeneratedModule, AslGeneratedSharedModule } from './generated/module';
-import { AslValidator } from './asl-validator';
+// import { AslValidator } from './asl-validator';
 
 /**
  * Declaration of custom services - add your own service classes here.
